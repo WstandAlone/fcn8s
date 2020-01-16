8ec95b98f3486dc98162ec5c25424ed33e35d706
荆志宝 <jingzb@fosun.com>
2020-01-16 23:01:49 +0800

    补充注释

--- fcn8s.py
+++ fcn8s.py
@@ -20,28 +20,40 @@ class FCN8s(nn.Module):
         )
 
     def __init__(self, n_class=21):
+        # 调用父类初始化方法
         super(FCN8s, self).__init__()
-        # conv1
+        # conv1 : stage
+        # 第一层卷积 in_channel:3   out_channel:64   kernel_size:3*3 单侧边缘填充size:100
         self.conv1_1 = nn.Conv2d(3, 64, 3, padding=100)
+        # relu激活函数
         self.relu1_1 = nn.ReLU(inplace=True)
+        # 第二层卷积 in_channel:64（与上一层卷积out_channel相同）  out_channel:64   kernel_size:3*3 单侧边缘填充size:1
         self.conv1_2 = nn.Conv2d(64, 64, 3, padding=1)
         self.relu1_2 = nn.ReLU(inplace=True)
+        # 最大池化层，2*2 strade=2池化后的结果为原来的一半
         self.pool1 = nn.MaxPool2d(2, stride=2, ceil_mode=True)  # 1/2
 
-        # conv2
+        # conv2 : stage
+        # 第一层卷积 in_channel:64（与上一层卷积out_channel相同）   out_channel:128   kernel_size:3*3 单侧边缘填充size:1
         self.conv2_1 = nn.Conv2d(64, 128, 3, padding=1)
         self.relu2_1 = nn.ReLU(inplace=True)
+        # 第二层卷积 in_channel:128（与上一层卷积out_channel相同）   out_channel:128   kernel_size:3*3 单侧边缘填充size:1
         self.conv2_2 = nn.Conv2d(128, 128, 3, padding=1)
         self.relu2_2 = nn.ReLU(inplace=True)
+        # 最大池化层，2*2 strade=2 池化后的结果为原来的一半
         self.pool2 = nn.MaxPool2d(2, stride=2, ceil_mode=True)  # 1/4
 
-        # conv3
+        # conv3 ：stage
+        # 第一层卷积 in_channel:128（与上一层卷积out_channel相同）   out_channel:256   kernel_size:3*3 单侧边缘填充size:1
         self.conv3_1 = nn.Conv2d(128, 256, 3, padding=1)
         self.relu3_1 = nn.ReLU(inplace=True)
+        # 第二层卷积 in_channel:256（与上一层卷积out_channel相同）   out_channel:256   kernel_size:3*3 单侧边缘填充size:1
         self.conv3_2 = nn.Conv2d(256, 256, 3, padding=1)
         self.relu3_2 = nn.ReLU(inplace=True)
+        # 第三层卷积 in_channel:256（与上一层卷积out_channel相同）   out_channel:256   kernel_size:3*3 单侧边缘填充size:1
         self.conv3_3 = nn.Conv2d(256, 256, 3, padding=1)
         self.relu3_3 = nn.ReLU(inplace=True)
+        # 最大池化层，2*2 strade=2 池化后的结果为原来的一半
         self.pool3 = nn.MaxPool2d(2, stride=2, ceil_mode=True)  # 1/8
 
         # conv4
@@ -65,21 +77,25 @@ class FCN8s(nn.Module):
         # fc6
         self.fc6 = nn.Conv2d(512, 4096, 7)
         self.relu6 = nn.ReLU(inplace=True)
+        # 正则化，随机使一半的神经元失去活性
         self.drop6 = nn.Dropout2d()
 
         # fc7
         self.fc7 = nn.Conv2d(4096, 4096, 1)
         self.relu7 = nn.ReLU(inplace=True)
         self.drop7 = nn.Dropout2d()
-
+        # n_class 分类数量
         self.score_fr = nn.Conv2d(4096, n_class, 1)
         self.score_pool3 = nn.Conv2d(256, n_class, 1)
         self.score_pool4 = nn.Conv2d(512, n_class, 1)
 
+        # 上采样2倍
         self.upscore2 = nn.ConvTranspose2d(
             n_class, n_class, 4, stride=2, bias=False)
+        # 上采样8倍
         self.upscore8 = nn.ConvTranspose2d(
             n_class, n_class, 16, stride=8, bias=False)
+        # 上采样2倍
         self.upscore_pool4 = nn.ConvTranspose2d(
             n_class, n_class, 4, stride=2, bias=False)
 
@@ -96,7 +112,7 @@ class FCN8s(nn.Module):
                 initial_weight = get_upsampling_weight(
                     m.in_channels, m.out_channels, m.kernel_size[0])
                 m.weight.data.copy_(initial_weight)
-
+    # 前向传播（调用各层）
     def forward(self, x):
         h = x
         h = self.relu1_1(self.conv1_1(h))
@@ -122,7 +138,7 @@ class FCN8s(nn.Module):
         h = self.relu5_1(self.conv5_1(h))
         h = self.relu5_2(self.conv5_2(h))
         h = self.relu5_3(self.conv5_3(h))
-        h = self.pool5(h)
+        h = self.pool5(h) # 1/32
 
         h = self.relu6(self.fc6(h))
         h = self.drop6(h)
@@ -131,26 +147,30 @@ class FCN8s(nn.Module):
         h = self.drop7(h)
 
         h = self.score_fr(h)
+        # 上采样2倍
         h = self.upscore2(h)
         upscore2 = h  # 1/16
-
+        # 用1*1卷积调整维度为n_class,如果要融合，大小深度都要相同
         h = self.score_pool4(pool4)
+        # 合并前进行裁剪
         h = h[:, :, 5:5 + upscore2.size()[2], 5:5 + upscore2.size()[3]]
         score_pool4c = h  # 1/16
-
+        # 第一次特征融合：现阶段是直接相加，以后会用到链接
         h = upscore2 + score_pool4c  # 1/16
+        # 上采样2倍
         h = self.upscore_pool4(h)
         upscore_pool4 = h  # 1/8
-
+        # 用1*1卷积调整维度为n_class,如果要融合，大小深度都要相同
         h = self.score_pool3(pool3)
         h = h[:, :,
               9:9 + upscore_pool4.size()[2],
               9:9 + upscore_pool4.size()[3]]
         score_pool3c = h  # 1/8
-
+        # 第二次特征融合
         h = upscore_pool4 + score_pool3c  # 1/8
-
+        # 上采样8倍（到此一共上采样了32倍，恢复到原图像大小）
         h = self.upscore8(h)
+        # 裁剪
         h = h[:, :, 31:31 + x.size()[2], 31:31 + x.size()[3]].contiguous()
 
         return h
